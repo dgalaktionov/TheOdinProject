7a9a3ec18d118e260a48d13196bd134e5bd7074e
DaGal <galaktionov@gmail.com>
2014-09-22 12:32:46 +0200

    Made model relationships (yay!)

--- private-events/app/models/event.rb
+++ private-events/app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :created_by, :class_name => "User"
+  has_many :invites
+  has_many :guests, :through => :invites
 end
--- private-events/app/models/user.rb
+++ private-events/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :created_events, :foreign_key => "created_by_id", :class_name => "Event"
+  has_many :invites, :foreign_key => :guest_id
+  has_many :invited_events, :through => :invites, :source => :event
   
   def to_s
     id
